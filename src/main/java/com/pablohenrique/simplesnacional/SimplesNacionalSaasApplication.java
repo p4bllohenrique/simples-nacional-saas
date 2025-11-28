9d366349c8fce7c1565de3de7903f7a5834a9962
Pablo Silva <p4bllohenrique@gmail.com>
2025-11-27 22:01:42 -0300

    chore: initial commit

---
+++ src/main/java/com/pablohenrique/simplesnacional/SimplesNacionalSaasApplication.java
@@ -0,0 +1,13 @@
+package com.pablohenrique.simplesnacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplesNacionalSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplesNacionalSaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablohenrique/simplesnacional/SimplesNacionalSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablohenrique.simplesnacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplesNacionalSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
